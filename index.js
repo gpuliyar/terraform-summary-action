ed0abaa5f7e5e94f49dffc45a9523a271d2d2fea
GP <gp@envoy.com>
2022-02-12 23:15:27 -0800

    add padding logic

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ const main = async() => {
     const tfPlanRows = tfJSON.resource_changes.map(resource => 
       [resource.address, resource.mode, resource.type, resource.name, resource.change.actions.join(', ')]
     );
+    const addressMaxLength = Math.max(...core(tfPlanRows).map(row => row[0].length));
+    const modeMaxLength = Math.max(...core(tfPlanRows).map(row => row[1].length));
+    const typeMaxLength = Math.max(...core(tfPlanRows).map(row => row[2].length));
+    const nameMaxLength = Math.max(...core(tfPlanRows).map(row => row[3].length));
 
     console.log('Terraform Plan Summary:');
-    console.log(`  Terraform Plan Summary Report - ${moment.utc().format('YYYY-MM-DD HH:mm:ss')}`);
-    console.log('--------------------------------------------------------------------------------');
-    console.log('Resources Change Summary List');
-    console.log('--------------------------------------------------------------------------------');
+    console.log(`>>> Terraform Plan Summary Report - ${moment.utc().format('YYYY-MM-DD HH:mm:ss')}`);
+    console.log('>>> Resources Change Summary List');
     tfPlanRows.forEach(element => {
-      console.log(element.join('\t'));
+      console.log(`${element[0].padEnd(addressMaxLength + 5)} | ${element[1].padEnd(modeMaxLength + 5)} | ${element[2].padEnd(typeMaxLength + 5)} | ${element[3].padEnd(nameMaxLength + 5)} | ${element[4]}`);
     });
   } catch(error) {
     console.log(error);
