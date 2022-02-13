7b748f73471a520715e95b3b6871e86aeb6165d7
GP <gp@envoy.com>
2022-02-12 19:22:52 -0800

    update

--- README.md
+++ README.md
@@ -5,18 +5,9 @@ Github Custom Action to generate a summary report of all the resources and its c
 
 Name | Description | Required
 --- | --- | ---
-`repository` | Git Repository name | `true`
-`pr-number` | PR number in the Git Repository | `true`
-`environment` | Deployment environment name | `true`
 `file-name` | Terraform json report file name | `true`
 
-## Outputs
-
-Name | Description
---- | ---
-`commit-id` | Reference to the commit id to fetch the terraform summary report from the pull request
-
-## Example Usage
+# Example Usage
 
 ```
 jobs:
@@ -26,23 +17,9 @@ jobs:
     - name: Set Terraform Summary Report
         uses: gpuliyar/terraform-summary-action@v1.0.0
         with:
-          # Repository name (Mandatory)
-          repository: gpuliyar/awesome-project
-
-          # PR Number  (Mandatory)
-          pr-number: 101
-
-          # Environment - name of the deployment environment. 
-          # Possible values - production, staging, uat, test, other, etc.
-          # Default value - staging
-          environment: production
-
           # Terraform json report file name to parse and generate the summary report.
           # Default value - tfplan-show.json
           file-name: tfplan-show.json
-        env:
-          # Github Token to access the pull request and update a comment to it.
-          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
 ```
 
 > `GITHUB_TOKEN` required to make necessary API calls to Github to set the Context and the relevant Status in the PR
--- index.js
+++ index.js
@@ -1,40 +1,25 @@
 const fs = require('fs');
-const json2md = require('json2md');
 const moment = require('moment');
 const core = require('@actions/core');
 
 const main = async() => {
   try {
-    const repository = core.getInput('repository', {required: true});
-    const prNumber = core.getInput('pr-number', {required: true});
     const fileName = core.getInput('file-name', {required: true});
-    const environment = core.getInput('environment', {required: true});
-    const token = process.env.GITHUB_TOKEN;
-
     const tfPlan = fs.readFileSync(fileName, 'utf8');
     const tfJSON = JSON.parse(tfPlan);
-    
-    const commentMD = json2md([
-      { h3: `Terraform Plan Summary - ${environment}` },
-      { blockquote: `Terraform Plan Summary Report - ${moment.utc().format('YYYY-MM-DD HH:mm:ss')}` },
-      { hr: "" },
-      { h4: "Resources Change Summary List" },
-      { table: 
-        { headers: ["Address", "Mode", "Type", "Name", "Actions"],
-        rows: tfJSON.resource_changes.map(resource => 
-          [`\`${resource.address}\``, `\`${resource.mode}\``, `\`${resource.type}\``, `\`${resource.name}\``, resource.change.actions.join(', ')]
-        ),
-      }},
-    ]);
 
-    const octokit = new github.getOctokit(token);
-    const prComment = octokit.rest.pulls.createReviewComment({
-      owner: repository.split('/')[0],
-      repo: repository.split('/')[1],
-      pull_number: prNumber,
-      body: commentMD,
+    const tfPlanRows = tfJSON.resource_changes.map(resource => 
+      [resource.address, resource.mode, resource.type, resource.name, resource.change.actions.join(', ')]
+    ).sort((a, b) => a[4].localeCompare(b[4]) ? 1 : -1);
+
+    console.log('Terraform Plan Summary:');
+    console.log(`  Terraform Plan Summary Report - ${moment.utc().format('YYYY-MM-DD HH:mm:ss')}`);
+    console.log('--------------------------------------------------------------------------------');
+    console.log('Resources Change Summary List');
+    console.log('--------------------------------------------------------------------------------');
+    tfPlanRows.forEach(element => {
+      console.log(element.join('\t'));
     });
-    console.log(prComment);
   } catch(error) {
     console.log(error);
   }
